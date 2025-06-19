9d2552597cae933e24471c6ca759b2573ccaf861
Pedro Marques <mvrqs.dev@gmail.com>
2025-06-19 04:58:54 -0300

    adicionando o use case 'fetchQuestionComments' e tests

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
 
@@ -14,6 +15,14 @@ export class InMemoryQuestionCommentsRepository implements QuestionCommentsRepos
 	async delete(questionComment: QuestionComment): Promise<void> {
 		const itemIndex = this.items.findIndex((item) => item.id === questionComment.id)
 		this.items.splice(itemIndex, 1)
+	}
+
+	async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+		const questionComments = this.items
+			.filter((item) => item.questionId.toString() === questionId)
+			.slice((page - 1) * 20, page * 20)
+
+		return questionComments
 	}
 
 	async create(questionComment: QuestionComment) {
