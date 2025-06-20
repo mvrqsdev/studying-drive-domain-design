079bc405c7bcd559dffa86c23a985bf0e3f1b1f8
Pedro Marques <mvrqs.dev@gmail.com>
2025-06-20 00:31:47 -0300

    aplicando conceitos de aggregate e watched-list e tests

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,4 +1,5 @@
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { CreateQuestionUseCase } from './create-question'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
@@ -15,9 +16,19 @@ describe('Create Question', () => {
 			authorId: '1',
 			title: 'Nova pergunta',
 			content: 'Conteúdo da pergunta',
+			attachmentsIds: ['1', '2'],
 		})
 
 		expect(result.isRight()).toBe(true)
 		expect(inMemoryQuestionsRepository.items[0]).toEqual(result.value?.question)
+		expect(inMemoryQuestionsRepository.items[0].attachments.currentItems).toHaveLength(2)
+		expect(inMemoryQuestionsRepository.items[0].attachments.currentItems).toEqual([
+			expect.objectContaining({
+				attachmentId: new UniqueEntityID('1'),
+			}),
+			expect.objectContaining({
+				attachmentId: new UniqueEntityID('2'),
+			}),
+		])
 	})
 })
--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,7 +1,8 @@
 import dayjs from 'dayjs'
-import { Entity } from '@/core/entities/entity'
+import { AggregateRoot } from '@/core/entities/aggregate-root'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
+import { QuestionAttachmentList } from '../question-attachment-list'
 import { Slug } from './value-objects/slug'
 
 export interface QuestionProps {
@@ -10,11 +11,12 @@ export interface QuestionProps {
 	title: string
 	content: string
 	slug: Slug
+	attachments: QuestionAttachmentList
 	createdAt: Date
 	updatedAt?: Date
 }
 
-export class Question extends Entity<QuestionProps> {
+export class Question extends AggregateRoot<QuestionProps> {
 	get authorId() {
 		return this.props.authorId
 	}
@@ -33,6 +35,14 @@ export class Question extends Entity<QuestionProps> {
 
 	get slug() {
 		return this.props.slug
+	}
+
+	get attachments() {
+		return this.props.attachments
+	}
+
+	set attachments(attachments: QuestionAttachmentList) {
+		this.props.attachments = attachments
 	}
 
 	get createdAt() {
@@ -73,13 +83,14 @@ export class Question extends Entity<QuestionProps> {
 	}
 
 	static create(
-		props: Optional<QuestionProps, 'createdAt' | 'slug'>,
+		props: Optional<QuestionProps, 'createdAt' | 'slug' | 'attachments'>,
 		id?: UniqueEntityID,
 	) {
 		const question = new Question(
 			{
 				...props,
 				slug: props.slug ?? Slug.createFromText(props.title),
+				attachments: props.attachments ?? new QuestionAttachmentList(),
 				createdAt: props.createdAt ?? new Date(),
 			},
 			id,
